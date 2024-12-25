b4b6263924de7f5526f11a611f9f038f9bd8b0b1
unknown <wadoudazer1234@gmail.com>
2024-12-25 22:50:22 +0100

    add PORT to main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from server import Server
+import os
 
 server = Server()
-app = server.app
-app.run()
+
+if __name__ == "__main__":
+    # Get the port from the environment variable or use 5000 as a fallback
+    port = int(os.environ.get("PORT", 5000))
+    server.app.run(host="0.0.0.0", port=port)
